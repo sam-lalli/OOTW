614c81f1a8690bd57ca4ee231fb8d93f45cd2ac7
Sam Lalli <2sam.lalli@gmail.com>
2021-07-15 20:13:15 -0600

    progress made, need to figure out how to turn url data into string

--- ootw/src/Image.js
+++ ootw/src/Image.js
@@ -14,6 +14,10 @@ const Image = props => {
 
     let buttonTitle = title ? "Close Description" : "View Description"
 
+    const isImage = image.url.pop() === "jpg"
+    //image.url.split().pop() === "jpg"
+    
+
     
     // https://youtu.be/q9ZCBR-onRw
     //conditional logic to decide if img or video
@@ -21,8 +25,10 @@ const Image = props => {
         <div className="Image">
             <h2 id="typewriter">{image.title}.</h2>
             <div classname="imgContainer">
-                <ReactPlayer className="player" url={image.url} playing={true} loop={true}/>
-                {/* <img id="image" src={image.url} alt={image.explaination}/> */}
+                {
+                    isImage ? <img id="image" src={image.url} alt={image.explaination}/>
+                    : <ReactPlayer className="player" url={image.url} playing={true} loop={true}/>
+                }                
             </div>
         
             <div>
